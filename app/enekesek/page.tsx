fdd6d0b2db4a9d989f5346b3b9b5ecc3b9cf3ebb
vilmao sky <vilmaosky@gmail.com>
2024-10-08 17:00:36 +0200

    REL+WEB+000.000.002+singer::slimrak

---
+++ app/enekesek/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ENEKESEK = () => {
+  return (
+    <main>
+        <div>RákRecords - Énekesek</div>
+    </main>
+  )
+}
+
+export default ENEKESEK
---
+++ app/enekesek/slimr-k/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const ENEKES_slimrák = () => {
+  return (
+    <main>
+      <section id='head-singer-main' className='font-extrabold text-5xl text-rose-600 text-center'>Slim Rák🦀🦀</section>
+      <section id='about-singer-main' className='border rounded border-[3px] border-rose-300 inline-block p-5'>
+        <div>
+          SINGER_ABOUT::SELFINPUT   
+        </div>
+      </section>
+    </main>
+  )
+}
+
+export default ENEKES_slimrák
